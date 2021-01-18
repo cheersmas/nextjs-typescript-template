45fc624e0f99875f43c4c684df8677199d48c537
cheersmas <shradhankr0028@gmail.com>
2021-01-18 15:17:42 +0530

    add eslint and editorconfig

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import type { AppProps } from 'next/app'
-import 'styles/globals.scss'
+import type { AppProps } from 'next/app';
+import { ReactElement } from 'react';
+import 'styles/globals.scss';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps): ReactElement {
+  return <Component {...pageProps} />;
 }
