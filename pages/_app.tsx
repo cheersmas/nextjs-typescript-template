32bec644f397a2850e838ed5aef8b92e54c4f2f9
cheersmas <shradhankr0028@gmail.com>
2021-01-18 16:43:52 +0530

    added prettier support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,6 @@ import type { AppProps } from 'next/app';
 import { ReactElement } from 'react';
 import 'styles/globals.scss';
 
-export default function MyApp({ Component, pageProps }: AppProps): ReactElement {
+export default function App({ Component, pageProps }: AppProps): ReactElement {
   return <Component {...pageProps} />;
 }
